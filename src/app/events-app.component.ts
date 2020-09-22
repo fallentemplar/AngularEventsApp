cddc93ed7ec11d0b09c1e7d0eed258c7c16e9544
Alexis Aguirre <aaaguirrer@hotmail.com>
2020-09-22 16:51:31 -0500

    Project created and added css

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Hello world</h1>
+    <img src="/assets/images/basic-shield.png">
+  `
+})
+export class EventsAppComponent {
+  title = 'EventsApp';
+}
