d52feded3f86710fbe83e6b9f033cbeecc137276
Alexis Aguirre <aaaguirrer@hotmail.com>
2020-09-22 20:59:41 -0500

    Added ng-for for event list

--- src/app/interfaces/IEvent.ts
+++ src/app/interfaces/IEvent.ts
@@ -1,3 +1,5 @@
+import { ISession } from './ISession';
+
 export interface IEvent {
     id: number,
     name: string,
@@ -9,5 +11,6 @@ export interface IEvent {
         address: string,
         city: string,
         country: string
-    }
+    },
+    sessions: ISession[]
 }
