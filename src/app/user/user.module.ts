d4cd5a529a02cdc8e61b68d31d195179f3a2d317
Alexis Aguirre <aaaguirrer@hotmail.com>
2020-09-23 21:52:01 -0500

    Added template-based form

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,14 +4,18 @@ import { RouterModule } from '@angular/router'
 
 import { userRoutes } from './user.routes'
 import { ProfileComponent } from './profile.component'
+import { LoginComponent } from './login.component'
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
+    declarations: [
+        ProfileComponent,
+        LoginComponent
+    ],
     imports: [
         CommonModule,
-        RouterModule.forChild(userRoutes)
-    ],
-    declarations: [
-        ProfileComponent
+        RouterModule.forChild(userRoutes),
+        FormsModule
     ],
     providers: [
 
