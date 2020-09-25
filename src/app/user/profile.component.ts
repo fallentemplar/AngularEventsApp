585a297675eb1dbd9170fd504bcbd8d15fe15e16
Alexis Aguirre <aaaguirrer@hotmail.com>
2020-09-25 17:35:51 -0500

    Added validation in reactive form

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,35 +1,50 @@
 import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Component({
-  templateUrl: "./profile.component.html"
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.css"]
 })
 export class ProfileComponent implements OnInit {
 
   profileForm: FormGroup
+
+  private firstName: FormControl
+  private lastName: FormControl
 
   constructor(private auth: AuthService, private router: Router) {
 
   }
 
   ngOnInit(): void {
-    let firstName = new FormControl(this.auth.currentUser.fistName);
-    let lastName = new FormControl(this.auth.currentUser.lastName);
+    this.firstName = new FormControl(this.auth.currentUser.fistName, Validators.required);
+    this.lastName = new FormControl(this.auth.currentUser.lastName, Validators.required);
     this.profileForm = new FormGroup({
-      firstName: firstName,
-      lastName: lastName
+      firstName: this.firstName,
+      lastName: this.lastName
     })
   }
 
   saveProfile(formValues): void {
+    if (!this.profileForm.invalid)
+      return
+
     this.auth.updateCurrentUser(formValues.firstName, formValues.lastName);
     this.router.navigate(["/events"]);
   }
 
   cancel(): void {
     this.router.navigate(["/events"]);
+  }
+
+  validateFirstName(): boolean {
+    return this.firstName.untouched || this.firstName.valid
+  }
+
+  validateLastName(): boolean {
+    return this.lastName.untouched || this.lastName.valid
   }
 
 }
