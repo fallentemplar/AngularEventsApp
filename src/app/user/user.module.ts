378ae7a6855dfc4b3996e235ede3f54ef5d079cc
Alexis Aguirre <aaaguirrer@hotmail.com>
2020-09-25 17:03:17 -0500

    Added Reactive Form

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -5,8 +5,7 @@ import { RouterModule } from '@angular/router'
 import { userRoutes } from './user.routes'
 import { ProfileComponent } from './profile.component'
 import { LoginComponent } from './login.component'
-import { FormsModule } from '@angular/forms'
-import { AuthService } from './auth.service'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
     declarations: [
@@ -16,7 +15,8 @@ import { AuthService } from './auth.service'
     imports: [
         CommonModule,
         RouterModule.forChild(userRoutes),
-        FormsModule
+        FormsModule,
+        ReactiveFormsModule
     ],
     providers: [
 
